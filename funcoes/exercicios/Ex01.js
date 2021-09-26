d581592e810581e8f231519d41fa674a060d1349
Nathanael Carauna <Nathanael.motta@midiavox.com.br>
2021-09-26 17:11:08 -0300

    Ex01 solved

---
+++ funcoes/exercicios/Ex01.js
@@ -0,0 +1,7 @@
+const fourOperations = (n1, n2) => {
+    console.log(`Soma: ${n1} + ${n2} = ${n1 + n2}`)
+    console.log(`Sbutração: ${n1} - ${n2} = ${n1 - n2}`)
+    console.log(`Soma: ${n1} / ${n2} = ${n1 / n2}`)
+    console.log(`Soma: ${n1} * ${n2} = ${n1 * n2}`)
+}
+fourOperations(4, 2)
