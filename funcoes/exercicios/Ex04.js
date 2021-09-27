ba301807b3df1ba1af2fbc7996ecb38c284de707
Nathanael Carauna <Nathanael.motta@midiavox.com.br>
2021-09-26 23:07:18 -0300

    Question 04 solved

---
+++ funcoes/exercicios/Ex04.js
@@ -0,0 +1,10 @@
+// 04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+// e o resto da divisão destes dois valores.
+
+function divide(dividendo, divisor) {
+    console.log(`Quociente: ${dividendo / divisor}`)
+    console.log(`Resto: ${dividendo % divisor}`)
+}
+
+divide(3, 2)
+divide(17, 4)
