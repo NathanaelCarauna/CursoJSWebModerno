286431818282d6d635bb853e28fc334514693a90
Nathanael Carauna <Nathanael.motta@midiavox.com.br>
2021-09-26 22:54:56 -0300

    Question added

--- funcoes/exercicios/Ex01.js
+++ funcoes/exercicios/Ex01.js
@@ -1,3 +1,6 @@
+// 01) Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+// multiplicação e divisão desses valores.
+
 const fourOperations = (n1, n2) => {
     console.log(`Soma: ${n1} + ${n2} = ${n1 + n2}`)
     console.log(`Sbutração: ${n1} - ${n2} = ${n1 - n2}`)
