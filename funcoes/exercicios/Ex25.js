b93da41028135311fb5a5d8572d5d78bd605a03b
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-10-05 10:54:13 -0300

    Ex25 solved

---
+++ funcoes/exercicios/Ex25.js
@@ -0,0 +1,9 @@
+// 25) Escrever um programa para exibir os números de 1 até 50 na tela.
+
+function countFrom1To50(){
+    for(let i = 1 ; i <= 50 ; i++){
+        console.log(i)
+    }
+}
+
+countFrom1To50()
