efe91ca80031270746c4e69c468f034fc475e2c2
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-10-06 11:14:49 -0300

    Implemented my own forEach

---
+++ arrays/myForEach.js
@@ -0,0 +1,9 @@
+Array.prototype.forEach2 = function(callback){
+    for(let i = 0; i< this.length; i++){
+        callback(this[i], i, this)
+    }
+}
+
+const aprovados = ['Fernando', 'Catarina', 'Julia']
+
+aprovados.forEach2((nome, index) => console.log(`${index +1}) ${nome}`))
