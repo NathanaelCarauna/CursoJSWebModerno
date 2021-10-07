073045d31c7ebfd7c1c200d78f1ba86853001ab6
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-10-07 09:23:25 -0300

    Ex01 solved

---
+++ fundamentals/exercises/Ex01.js
@@ -0,0 +1,12 @@
+// 01) Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a
+//     função) e com ponto de exclamação "!" no final.
+//     Exemplos:
+//     cumprimentar("Leonardo") // retornará "Olá, Leonardo!"
+//     cumprimentar("Maria") // retornará "Olá, Maria!"
+
+function greet(name){
+    return `Hello, ${name}!`;
+}
+
+console.log(greet("Leonardo"));
+console.log(greet("Maria"));
