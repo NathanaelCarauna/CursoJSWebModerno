37c3b3d375e4f87eb04a00c7b616b205250aea15
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-10-07 09:31:37 -0300

    Ex04 solved

---
+++ fundamentals/exercises/Ex04.js
@@ -0,0 +1,26 @@
+// 04) Crie uma função que recebe um número (de 1 a 12 e retorne o mês correspondente como uma string. Por
+//     exemplo, se a entrada for 2, a função deverá retornar "fevereiro", pois este é o 2° mês.
+//     Exemplos:
+//     nomeDoMes(1) // retornará "janeiro"
+//     nomeDoMes(4) // retornará "abril"
+    
+function monthName(number){
+    switch(number){
+        case 1: return 'January'
+        case 2: return 'February'
+        case 3: return 'March'
+        case 4: return 'April'
+        case 5: return 'May'
+        case 6: return 'June'
+        case 7: return 'July'
+        case 8: return 'August'
+        case 9: return 'September'
+        case 10: return 'October'
+        case 11: return 'November'
+        case 12: return 'December'
+        default: return 'Invalid input'
+    }    
+}
+
+console.log(monthName(1))
+console.log(monthName(4))
